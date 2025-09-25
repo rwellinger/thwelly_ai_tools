e47474998caf0408b1488f2f1d669eea88af54fd
thWelly <wellytone99@gmail.com>
2025-09-25 18:03:54 +0200

    api doku added

--- aiproxysrv/src/api/app.py
+++ aiproxysrv/src/api/app.py
@@ -53,7 +53,7 @@ def create_app():
     # OpenAPI/Swagger Configuration
     spec = APISpec(
         title='thWellys AI-Proxy API',
-        version='1.3.2',
+        version='1.4.0',
         openapi_version='3.0.2',
         info=dict(
             description='API für AI-Services: Bildgenerierung, Musikgenerierung und Chat-Integration',
