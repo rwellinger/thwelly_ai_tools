447e07bc7cade0971faddf286639eb586496e8da
thWelly <wellytone99@gmail.com>
2025-09-30 14:45:33 +0200

    nginx fix

--- aiproxysrv/src/celery_app/__init__.py
+++ aiproxysrv/src/celery_app/__init__.py
@@ -2,11 +2,8 @@
 Celery App Package
 Exportiert die wichtigsten Objekte für einfachen Import
 """
-# IMPORTANT: Initialize loguru BEFORE importing celery_config
-from utils.logger import logger  # noqa: F401
-
 from .celery_config import celery_app
 from .tasks import generate_song_task, generate_instrumental_task
-from .slot_manager import get_slot_status
+from .slot_manager import get_slot_manager
 
-__all__ = ['celery_app', 'generate_song_task', 'generate_instrumental_task', 'get_slot_status']
+__all__ = ['celery_app', 'generate_song_task', 'generate_instrumental_task', 'get_slot_manager']
--- aiproxysrv/src/celery_app/celery_config.py
+++ aiproxysrv/src/celery_app/celery_config.py
@@ -3,8 +3,10 @@ Celery Konfiguration - Zentraler Import-Point für Celery
 """
 import logging
 from celery import Celery
-from celery.signals import setup_logging
-from config.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
+from celery.signals import setup_logging, worker_process_init
+from config.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND, LOG_LEVEL
+
+# IMPORTANT: Import logger FIRST to initialize loguru before Celery sets up its logging
 from utils.logger import CeleryInterceptHandler, logger
 
 # Celery App erstellen
@@ -30,21 +32,39 @@ celery_app.conf.update(
 )
 
 
-# Celery Logging auf loguru umleiten
-@setup_logging.connect
-def configure_celery_logging(**kwargs):
-    """Celery's logging komplett auf loguru umleiten"""
+def _configure_loguru_for_celery():
+    """Configure all Celery loggers to use loguru"""
+    log_level = getattr(logging, LOG_LEVEL.upper(), logging.INFO)
 
     # Celery Root Logger
     celery_logger = logging.getLogger('celery')
     celery_logger.handlers = [CeleryInterceptHandler()]
-    celery_logger.setLevel(logging.DEBUG)
+    celery_logger.setLevel(log_level)
     celery_logger.propagate = False
 
     # Celery Task Logger
     task_logger = logging.getLogger('celery.task')
     task_logger.handlers = [CeleryInterceptHandler()]
-    task_logger.setLevel(logging.DEBUG)
+    task_logger.setLevel(log_level)
     task_logger.propagate = False
 
-    logger.info("Celery logging configured to use loguru")
+    # Root logger for all other modules
+    root_logger = logging.getLogger()
+    root_logger.handlers = [CeleryInterceptHandler()]
+    root_logger.setLevel(log_level)
+
+    logger.info(f"Celery logging configured to use loguru (level={LOG_LEVEL})")
+
+
+# Celery Logging auf loguru umleiten (wird beim Worker-Start aufgerufen)
+@setup_logging.connect
+def configure_celery_logging(**kwargs):
+    """Celery's logging komplett auf loguru umleiten"""
+    _configure_loguru_for_celery()
+
+
+# Wird bei jedem Worker-Process aufgerufen (wichtig für prefork)
+@worker_process_init.connect
+def configure_worker_logging(**kwargs):
+    """Configure logging for each worker process"""
+    _configure_loguru_for_celery()
