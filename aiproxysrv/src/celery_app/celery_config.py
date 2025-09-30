2ba5b5572d47c91e53e8bc2d0f72f7535918f7ca
thWelly <wellytone99@gmail.com>
2025-09-30 14:14:05 +0200

    nginx fix

--- aiproxysrv/src/celery_app/celery_config.py
+++ aiproxysrv/src/celery_app/celery_config.py
@@ -1,8 +1,11 @@
 """
 Celery Konfiguration - Zentraler Import-Point für Celery
 """
+import logging
 from celery import Celery
+from celery.signals import setup_logging
 from config.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
+from utils.logger import CeleryInterceptHandler, logger
 
 # Celery App erstellen
 celery_app = Celery(
@@ -25,3 +28,23 @@ celery_app.conf.update(
     # Tasks auch explizit importieren beim App-Start
     imports=['celery_app.tasks']
 )
+
+
+# Celery Logging auf loguru umleiten
+@setup_logging.connect
+def configure_celery_logging(**kwargs):
+    """Celery's logging komplett auf loguru umleiten"""
+
+    # Celery Root Logger
+    celery_logger = logging.getLogger('celery')
+    celery_logger.handlers = [CeleryInterceptHandler()]
+    celery_logger.setLevel(logging.DEBUG)
+    celery_logger.propagate = False
+
+    # Celery Task Logger
+    task_logger = logging.getLogger('celery.task')
+    task_logger.handlers = [CeleryInterceptHandler()]
+    task_logger.setLevel(logging.DEBUG)
+    task_logger.propagate = False
+
+    logger.info("Celery logging configured to use loguru")
